2a99cf1b2b48820363fa1eacfaf66e1960f6f4e1
Maxwell Dubin <maxdhs@gmail.com>
2018-11-06 11:45:05 -0800

    Add multiple divs to show/hide story elements

--- js/script.js
+++ js/script.js
@@ -1,61 +1,73 @@
 
 var healthCounter = 3;
 
-function itsComing() {
-  var itsComing = document.getElementById("introMusic");
-  itsComing.play();
-  
-}
-/*
-function stopItsComing() {
-  var stopItsComing = document.getElementById("introMusic");
-  stopItsComing.pause();
-}
-*/
-
 $(document).ready(function () {
   $("#room0").slideDown();
   $("#room0toRoom1").click(function() {
     $(".room").hide();
     $("#room1").slideDown();
-    $("#healthDisplay").text(healthCounter);
+    $(".healthDisplay").text(healthCounter);
   });
 
   $("#GameOver").click(function() {
     $(".room").hide();
     $("#gameOver").slideDown();
+    $(".healthDisplay").text(healthCounter);
   });
 
   $("#gameOverToRoom0").click(function() {
     $(".room").hide();
     $("#room0").slideDown();
+    $(".healthDisplay").text(healthCounter);
   });
   
   $("#room1toRoom2").click(function() {
     $(".room").hide();
     $("#room2").slideDown();
+    $(".healthDisplay").text(healthCounter);
   });
 
   $("#room1toRoom2-damage").click(function() {
     $(".room").hide();
-    $("#room2-damage").slideDown();
     healthCounter--;
+    $("#room2-damage").slideDown();
+    $(".healthDisplay").text(healthCounter);
   });
 
   $("#room2toRoom3").click(function() {
     $(".room").hide();
     $("#room3").slideDown();
+    $(".healthDisplay").text(healthCounter);
   });
 
   $("#room2toRoom3-damage").click(function() {
     $(".room").hide();
     $("#room3-damage").slideDown();
-    healthCounter--;
+    $(".healthDisplay").text(healthCounter);
+  });
+  
+  $("#room3toRoom4-damage").click(function() {
+    $(".room").hide();
+    $("#room4-damage").slideDown();
+    $(".healthDisplay").text(healthCounter);
   });
 
   $("#room3toRoom4").click(function() {
     $(".room").hide();
     $("#room4").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+
+  $("#room4toRoom5").click(function() {
+    $(".room").hide();
+    $("#room5").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+
+  $("#room4toRoom5-damage").click(function() {
+    $(".room").hide();
+    $("#room5-damage").slideDown();
+    $(".healthDisplay").text(0);
   });
 
 
