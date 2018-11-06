95ac51d8e745e0e91dd1c2b91099e216a45aea1e
Maxwell Dubin <maxdhs@gmail.com>
2018-11-06 10:27:57 -0800

    initial files for a horror webpage

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+var healthCounter = 3;
+
+function itsComing() {
+  var itsComing = document.getElementById("introMusic");
+  itsComing.play();
+  
+}
+/*
+function stopItsComing() {
+  var stopItsComing = document.getElementById("introMusic");
+  stopItsComing.pause();
+}
+*/
+
+$(document).ready(function () {
+  $("#room0").slideDown();
+  $("#room0toRoom1").click(function() {
+    $(".room").hide();
+    $("#room1").slideDown();
+    $("#healthDisplay").text(healthCounter);
+  });
+
+  $("#GameOver").click(function() {
+    $(".room").hide();
+    $("#gameOver").slideDown();
+  });
+
+  $("#gameOverToRoom0").click(function() {
+    $(".room").hide();
+    $("#room0").slideDown();
+  });
+  
+  $("#room1toRoom2").click(function() {
+    $(".room").hide();
+    $("#room2").slideDown();
+  });
+
+  $("#room1toRoom2-damage").click(function() {
+    $(".room").hide();
+    $("#room2-damage").slideDown();
+    healthCounter--;
+  });
+
+  $("#room2toRoom3").click(function() {
+    $(".room").hide();
+    $("#room3").slideDown();
+  });
+
+  $("#room2toRoom3-damage").click(function() {
+    $(".room").hide();
+    $("#room3-damage").slideDown();
+    healthCounter--;
+  });
+
+  $("#room3toRoom4").click(function() {
+    $(".room").hide();
+    $("#room4").slideDown();
+  });
+
+
+});
+
+
