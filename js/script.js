ed456c6226954a8f405da64584bbe3f642cb4839
Guest User <Guest@epicodus-100.local>
2018-11-07 10:37:48 -0800

    Add attic and basement endings

--- js/script.js
+++ js/script.js
@@ -2,59 +2,13 @@
 var healthCounter = 3;
 
 
-function itsComing() {
-  var itsComing = document.getElementById("introMusic");
-  itsComing.play();
-}
-
-function doorOpen() {
-  var dooropen = document.getElementById("doorOpen");
-  dooropen.play();
-}
-function bathroomNoise() {
-  var bathroomNoise = document.getElementById("bathroomNoise");
-  bathroomNoise.play();
-}
-function footSteps1() {
-  var footSteps1 = document.getElementById("footSteps1");
-  footSteps1.play();
-}
-function heartBeat() {
-  var heartBeat = document.getElementById("heartBeat");
-  heartBeat.play();
-}
-function bellSound() {
-  var bellSound = document.getElementById("bellSound");
-  bellSound.play();
-}
-function lidCreak() {
-  var lidCreak = document.getElementById("lidCreak");
-  lidCreak.play();
-}
-function lightning() {
-  var lightning = document.getElementById("lightning");
-  lightning.play();
-}
-function windowNoise() {
-  var windowNoise = document.getElementById("windowNoise");
-  windowNoise.play();
-}
-/*
-function stopItsComing() {
-  var stopItsComing = document.getElementById("introMusic");
-  stopItsComing.pause();
-}
-*/
-
-
-
 $(document).ready(function () {
   $("#room0").slideDown();
-
   $("#room0toRoom1").click(function() {
     $(".room").hide();
     $("#bedroomS1").slideDown();
     $(".healthDisplay").text(healthCounter);
+    lightning();
   });
 
 
@@ -160,6 +114,48 @@ $(document).ready(function () {
   });
 
   $("#officeS4btn1").click(function() {
+    $(".room").hide();
+    $("#gameOver").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+
+  $("#officeS2btn1").click(function() {
+    $(".room").hide();
+    $("#atticS1").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+  $("#atticS1btn1").click(function() {
+    $(".room").hide();
+    $("#atticS2").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+  $("#atticS1btn2").click(function() {
+    $(".room").hide();
+    $("#basementS1").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+
+  $("#basementS1btn1").click(function() {
+    $(".room").hide();
+    $("#basementEnding").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+  $("#basementS1btn2").click(function() {
+    $(".room").hide();
+    $("#basementGameOver").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+  $("#atticS2btn1").click(function() {
+    $(".room").hide();
+    $("#gameOver").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+  $("#basementEndingbtn1").click(function() {
+    $(".room").hide();
+    $("#gameOver").slideDown();
+    $(".healthDisplay").text(healthCounter);
+  });
+  $("#basementGameOverbtn1").click(function() {
     $(".room").hide();
     $("#gameOver").slideDown();
     $(".healthDisplay").text(healthCounter);
